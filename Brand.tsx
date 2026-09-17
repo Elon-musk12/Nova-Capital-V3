c6b001c66ebd8f6d593b9e41f1c455b59fc5196d
Elon-musk12 <seyjayaffiliate@gmail.com>
2026-09-17 14:35:46 +0100

    Delete Brand.tsx

--- Brand.tsx
+++
@@ -1,2 +0,0 @@
-import Link from 'next/link';
-export function Brand(){return <Link href="/" className="brand"><span className="brand-mark">Λ</span><span>NOVA CAPITAL<small>INVEST WITH CLARITY. BUILD WITH PURPOSE.</small></span></Link>}
