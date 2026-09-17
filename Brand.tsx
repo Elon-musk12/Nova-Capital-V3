b6f53a41ea441c76516099cd8ee3153dd36f1768
Elon-musk12 <seyjayaffiliate@gmail.com>
2026-09-17 14:32:42 +0100

    Add files via upload

---
+++ Brand.tsx
@@ -0,0 +1,2 @@
+import Link from 'next/link';
+export function Brand(){return <Link href="/" className="brand"><span className="brand-mark">Λ</span><span>NOVA CAPITAL<small>INVEST WITH CLARITY. BUILD WITH PURPOSE.</small></span></Link>}
