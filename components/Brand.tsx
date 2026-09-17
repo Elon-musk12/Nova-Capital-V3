fd2a5424a47beef02ff5e5ce5e599a1203b87484
Elon-musk12 <seyjayaffiliate@gmail.com>
2026-09-17 14:38:35 +0100

    Create Brand.tsx

---
+++ components/Brand.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export function Brand() {
+  return (
+    <Link href="/" className="brand">
+      <span className="brand-mark">Λ</span>
+      <span>
+        NOVA CAPITAL
+        <small>INVEST WITH CLARITY. BUILD WITH PURPOSE.</small>
+      </span>
+    </Link>
+  );
+}
